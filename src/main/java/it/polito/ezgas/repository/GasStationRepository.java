2d1bb556a7199ddc5d80dda2de3088ac33c7a35b
Marzia Cava s234128 <s234128@studenti.polito.it>
2020-05-10 15:32:02 +0200

    feat: modified GasStationRepository

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -3,7 +3,6 @@ package it.polito.ezgas.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.polito.ezgas.entity.GasStation;
@@ -12,10 +11,10 @@ import it.polito.ezgas.entity.GasStation;
 public interface GasStationRepository extends JpaRepository<GasStation, Integer>{
 	
 	public List<GasStation> findByGasStationId(Integer gasStationId);
-	public List<GasStation> findByHasMethane();
-	public List<GasStation> findByHasDiesel();
-	public List<GasStation> findByHasSuper();
-	public List<GasStation> findByHasSuperPlus();
-	public List<GasStation> findByHasGas();
+	public List<GasStation> findByHasMethaneTrue();
+	public List<GasStation> findByHasDieselTrue();
+	public List<GasStation> findByHasSuperTrue();
+	public List<GasStation> findByHasSuperPlusTrue();
+	public List<GasStation> findByHasGasTrue();
 	
 }
