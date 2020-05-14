c14900d4f35ae9a777dc0fda7aa2eadaedca1ea6
MANUEL SABELLI s275945 <s275945@studenti.polito.it>
2020-05-14 19:03:24 +0200

    feat: GasStationRepository.java correction queries

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -3,6 +3,7 @@ package it.polito.ezgas.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.polito.ezgas.entity.GasStation;
@@ -12,9 +13,14 @@ public interface GasStationRepository extends JpaRepository<GasStation, Integer>
 	
 	public List<GasStation> findByGasStationId(Integer gasStationId);
 	public List<GasStation> findByHasMethaneTrue();
+	public List<GasStation> findByCarSharing(String carSharing);
 	public List<GasStation> findByHasDieselTrue();
 	public List<GasStation> findByHasSuperTrue();
 	public List<GasStation> findByHasSuperPlusTrue();
 	public List<GasStation> findByHasGasTrue();
 	
+	@Query(value = "SELECT gasStationId, gasStationName, gasStationAddress, hasDiesel, hasSuper, hasSuperPlus, hasGas, hasMethane, carSharing, "
+			+ "lat, lon, dieselPrice, superPrice, superPlusPrice, gasPrice, methanePrice, reportUser, reportTimestamp, reportDependability"
+			+ " FROM GasStation WHERE (lat - ?1) <= 1000/1.41 AND (lon - ?2) <= 1000/1.41")
+	public List<Object> findByLatAndLon(Double lat, Double lon);
 }
