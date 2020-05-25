37e7223ad1749c8ccabe22421aa07548deda444a
Marzia Cava s234128 <s234128@studenti.polito.it>
2020-05-25 14:11:46 +0200

    feat: added Repositories in TestSuite

--- src/test/java/it/polito/ezgas/TestSuite.java
+++ src/test/java/it/polito/ezgas/TestSuite.java
@@ -8,6 +8,8 @@ import it.polito.ezgas.converter.*;
 import it.polito.ezgas.dto.GasStationDtoTests;
 import it.polito.ezgas.dto.UserDtoTests;
 import it.polito.ezgas.entity.*;
+import it.polito.ezgas.repository.GasStationRepositoryTest;
+import it.polito.ezgas.repository.UserRepositoryTests;
 import it.polito.ezgas.service.*;
 import it.polito.ezgas.utils.*;
 
@@ -17,7 +19,8 @@ import it.polito.ezgas.utils.*;
 	GasStationTest.class, UserTests.class, UtilityTest.class,
 	GasStationServiceTest.class, UserServiceTests.class,
 	GasStationConverterTest.class, UserConverterTests.class,
-	GasStationDtoTests.class, UserDtoTests.class
+	GasStationDtoTests.class, UserDtoTests.class,
+	GasStationRepositoryTest.class, UserRepositoryTests.class
 })
 public class TestSuite {
 
