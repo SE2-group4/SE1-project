a32a2e5fe4ae816bdbadfbdbda6d1124605ce715
Marzia Cava s234128 <s234128@studenti.polito.it>
2020-05-28 18:15:15 +0200

    feat: added database for GUI testing

--- src/main/java/it/polito/ezgas/BootEZGasApplication.java
+++ src/main/java/it/polito/ezgas/BootEZGasApplication.java
@@ -23,23 +23,59 @@ public class BootEZGasApplication {
 	public void setupDbWithData() throws SQLException {
 				
 		Connection conn = DriverManager.getConnection("jdbc:h2:./data/memo", "sa", "password");
-		//da togliere
-		conn.close();
-				
+		
 		User user = new User("admin", "admin", "admin@ezgas.com", 5);
 		user.setAdmin(true);
 		
+		// da commentare da qui 
 		/*PreparedStatement st = null;
+		PreparedStatement u1 = null;
+		PreparedStatement u2 = null;
+		PreparedStatement gs1 = null;
+		PreparedStatement gs2 = null;
+		PreparedStatement gs3 = null;
+		PreparedStatement del1 = null;
+		PreparedStatement del2 = null;
 		
 		try {
-			st = conn.prepareStatement("INSERT INTO USER (admin, email, password, reputation, user_name) VALUES (?, ?, ?, ?, ?)");
-			st.setBoolean(1, user.getAdmin());
-			st.setString(2, user.getEmail());
-			st.setString(3, user.getPassword());
-			st.setLong(4, user.getReputation());
-			st.setString(5, user.getUserName());
 			
+			del1 = conn.prepareStatement("DELETE FROM USER");
+			del1.execute();
+			del2 = conn.prepareStatement("DELETE FROM GAS_STATION");
+			del2.execute();
+			
+			st = conn.prepareStatement("INSERT INTO USER (user_id, admin, email, password, reputation, user_name) "
+									 + "VALUES (1, true, 'admin@ezgas.com', 'admin', 2, 'admin')");
 			st.execute();
+			
+			u1 = conn.prepareStatement("INSERT INTO USER (user_id, admin, email, password, reputation, user_name) "
+									 + "VALUES (2, false, 'user1@ezgas.com', 'user1', 0, 'user1');");
+			u1.execute();
+			
+			u2 = conn.prepareStatement("INSERT INTO USER (user_id, admin, email, password, reputation, user_name) "
+									 + "VALUES (3, false, 'user2@ezgas.com', 'user2', 0, 'user2');");
+			u2.execute();
+			
+			gs1 = conn.prepareStatement("INSERT INTO USER (gas_station_id, car_sharing, diesel_price, gas_price, gas_station_address,"
+									  + "gas_station_name, has_diesel, has_gas, has_methane, has_super, has_super_plus, lat, lon, methane_price,"
+									  + "report_dependability, report_timestamp, report_user, super_plus_price, super_price, user_id) "
+									  + "VALUES (1, 'Enjoy', 0, -1, 'Via Rocciamelone Caselle Torinese Piemont Italy', 'Station1', true, false,"
+									  + "false, true, false, 45.1635676, 7.6647799, -1, 0, null, -1, -1, 0, null);");
+			gs1.execute();
+			
+			gs2 = conn.prepareStatement("INSERT INTO USER (gas_station_id, car_sharing, diesel_price, gas_price, gas_station_address,"
+									  + "gas_station_name, has_diesel, has_gas, has_methane, has_super, has_super_plus, lat, lon, methane_price,"
+									  + "report_dependability, report_timestamp, report_user, super_plus_price, super_price, user_id) "
+									  + "VALUES (2, 'Car2Go', -1, 3, 'Via Roma Turin Piemont Italy', 'Station2', false, true,"
+									  + "true, false, true, 45.0705111, 7.6845806, -1, 0, 'Thu May 28 17:11:56 CEST 2020', 2, 3, -1, 2);");
+			gs2.execute();
+			
+			gs3 = conn.prepareStatement("INSERT INTO USER (gas_station_id, car_sharing, diesel_price, gas_price, gas_station_address,"
+									  + "gas_station_name, has_diesel, has_gas, has_methane, has_super, has_super_plus, lat, lon, methane_price,"
+									  + "report_dependability, report_timestamp, report_user, super_plus_price, super_price, user_id) "
+									  + "VALUES (3, null, 0, -1, 'Via Rocciamelone Caselle Torinese Piemont Italy', 'Station3', true, false,"
+									  + "false, true, false, 45.1635676, 7.6647799, -1, 0, null, -1, -1, 0, null);");
+			gs3.execute();
 			
 			System.out.println(user.getEmail());
 		} 
