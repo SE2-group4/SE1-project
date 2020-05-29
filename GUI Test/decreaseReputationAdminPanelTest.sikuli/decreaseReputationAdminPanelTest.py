81c6e83c7818f64d8d5603ed164aa475481ae909
COSIMO CHETTA s271819 <s271819@studenti.polito.it>
2020-05-29 20:03:31 +0200

    feat: decrease and increase reputation GUI test

---
+++ GUI Test/decreaseReputationAdminPanelTest.sikuli/decreaseReputationAdminPanelTest.py
@@ -0,0 +1,22 @@
+if(not exists("1590765672108-1.png")):
+    click("1590765489265-2.png")
+wait("1590765672108-1.png")
+while(not exists("1590772570205.png")):
+    wheel(WHEEL_DOWN, 5)
+    sleep(0.1)
+
+search = find("1590772570205.png")
+click(search.find("1590772624592.png"))
+type("Via Roma Turin Piemont Italy")
+sleep(3)
+type(Key.DOWN + Key.ENTER)
+click(search.find("1590772775981.png"))
+wheel(WHEEL_DOWN, 2)
+sleep(0.5)
+click("1590775185385.png")
+sleep(0.5)
+while(not exists("1590774223598.png")):
+    wheel(WHEEL_UP, 3)
+    sleep(0.1)
+admin = find("1590775210889.png")
+admin.find("1590775204293.png")
