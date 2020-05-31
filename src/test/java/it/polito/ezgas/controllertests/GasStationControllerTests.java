fdc5ddff072b599747e22605f18243ab819c9036
MANUEL SABELLI s275945 <s275945@studenti.polito.it>
2020-05-31 23:18:56 +0200

    feat: byProximity fix

--- src/test/java/it/polito/ezgas/controllertests/GasStationControllerTests.java
+++ src/test/java/it/polito/ezgas/controllertests/GasStationControllerTests.java
@@ -24,12 +24,12 @@ import it.polito.ezgas.dto.GasStationDto;
 import it.polito.ezgas.dto.UserDto;
 
 public class GasStationControllerTests {
-	
+
 	static GasStationDto gsDto;
-	
+
 	@BeforeAll
 	static public void setup() throws SQLException {
-		
+
 		gsDto = new GasStationDto();
 		gsDto.setGasStationId(1);
 		gsDto.setCarSharing("Enjoy");
@@ -52,29 +52,29 @@ public class GasStationControllerTests {
 		gsDto.setSuperPrice(0);
 		gsDto.setUserDto(null);
 	}
-	
+
 	/*@Test
 	public void testGetAllGasStation() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getAllGasStations");
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		String jsonFromResponse = EntityUtils.toString(response.getEntity());
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
-		
+
 		//assert(gs_array.length == 6);
 	}*/
-	
+
 	/*@Test
 	public void testGetGasStationById() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getGasStation/1");
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		String jsonFromResponse = EntityUtils.toString(response.getEntity());
-				
+
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		GasStationDto gs = mapper.readValue(jsonFromResponse, GasStationDto.class);
-				
+
 		assertNotNull(gs);
         assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
         assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
@@ -91,82 +91,49 @@ public class GasStationControllerTests {
         assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
         assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
         assertEquals(gs.getLat(), gsDto.getLat());
-        assertEquals(gs.getLon(), gsDto.getLon());	
-        
+        assertEquals(gs.getLon(), gsDto.getLon());
+
 	}
-	
+
 	@Test
 	public void testSaveGasStation() throws ClientProtocolException, IOException {
 		HttpPost request = new HttpPost("http://localhost:8080/gasstation/saveGasStation");
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 	    //Converting the Object to JSONString
 	    String jsonString = mapper.writeValueAsString(gsDto);
 	    StringEntity json = new StringEntity(jsonString);
 	    request.setHeader("Content-Type", "application/json");
 	    request.setEntity(json);
-	    
+
 	    HttpResponse response = HttpClientBuilder.create().build().execute(request);
-	    
+
 		assertEquals(response.getStatusLine().getStatusCode(), 200);
-        
+
 	}
-	
+
 	@Test
 	public void testDeleteGasStation() throws ClientProtocolException, IOException {
 		HttpDelete request = new HttpDelete("http://localhost:8080/gasstation/deleteGasStation/3");
-	    
+
 	    HttpResponse response = HttpClientBuilder.create().build().execute(request);
-	    
+
 		assertEquals(response.getStatusLine().getStatusCode(), 200);
-        
+
 	}
-	
+
 	@Test
 	public void testGetGasStationByGasolineType() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/searchGasStationByGasolineType/Super");
-	    
-		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
-		String jsonFromResponse = EntityUtils.toString(response.getEntity());
-						
-		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
-		GasStationDto gs = gs_array[0];
-				
-		assertNotNull(gs);
-        assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
-        assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
-        assertEquals(gs.getGasStationAddress(), gsDto.getGasStationAddress());
-        assertEquals(gs.getHasDiesel(), gsDto.getHasDiesel());
-        assertEquals(gs.getHasGas(), gsDto.getHasGas());
-        assertEquals(gs.getHasMethane(), gsDto.getHasMethane());
-        assertEquals(gs.getHasSuper(), gsDto.getHasSuper());
-        assertEquals(gs.getHasSuperPlus(), gsDto.getHasSuperPlus());
-        assertEquals(gs.getCarSharing(), gsDto.getCarSharing());
-        assertEquals(gs.getDieselPrice(), gsDto.getDieselPrice());
-        assertEquals(gs.getGasPrice(), gsDto.getGasPrice());
-        assertEquals(gs.getMethanePrice(), gsDto.getMethanePrice());
-        assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
-        assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
-        assertEquals(gs.getLat(), gsDto.getLat());
-        assertEquals(gs.getLon(), gsDto.getLon());	
-	}
-	
-	@Test
-	public void testGetGasStationByProximity() throws ClientProtocolException, IOException {
-		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/searchGasStationByProximity/45.1635676/7.6647799");
 
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		String jsonFromResponse = EntityUtils.toString(response.getEntity());
-		
-		System.out.println(jsonFromResponse);
-				
+
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
 		GasStationDto gs = gs_array[0];
-				
+
 		assertNotNull(gs);
         assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
         assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
@@ -183,23 +150,56 @@ public class GasStationControllerTests {
         assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
         assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
         assertEquals(gs.getLat(), gsDto.getLat());
-        assertEquals(gs.getLon(), gsDto.getLon());	
+        assertEquals(gs.getLon(), gsDto.getLon());
 	}
-	
+
+	@Test
+	public void testGetGasStationByProximity() throws ClientProtocolException, IOException {
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/searchGasStationByProximity/45.1635676/7.6647799/");
+
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		System.out.println(jsonFromResponse);
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
+		GasStationDto gs = gs_array[0];
+
+		assertNotNull(gs);
+        assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
+        assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
+        assertEquals(gs.getGasStationAddress(), gsDto.getGasStationAddress());
+        assertEquals(gs.getHasDiesel(), gsDto.getHasDiesel());
+        assertEquals(gs.getHasGas(), gsDto.getHasGas());
+        assertEquals(gs.getHasMethane(), gsDto.getHasMethane());
+        assertEquals(gs.getHasSuper(), gsDto.getHasSuper());
+        assertEquals(gs.getHasSuperPlus(), gsDto.getHasSuperPlus());
+        assertEquals(gs.getCarSharing(), gsDto.getCarSharing());
+        assertEquals(gs.getDieselPrice(), gsDto.getDieselPrice());
+        assertEquals(gs.getGasPrice(), gsDto.getGasPrice());
+        assertEquals(gs.getMethanePrice(), gsDto.getMethanePrice());
+        assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
+        assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
+        assertEquals(gs.getLat(), gsDto.getLat());
+        assertEquals(gs.getLon(), gsDto.getLon());
+	}
+
 	@Test
 	public void testGetGasStationWithCoordinates() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getGasStationsWithCoordinates/45.1635676/7.6647799/Diesel/Enjoy");
 
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		String jsonFromResponse = EntityUtils.toString(response.getEntity());
-		
+
 		System.out.println(jsonFromResponse);
-				
+
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
 		GasStationDto gs = gs_array[0];
-				
+
 		assertNotNull(gs);
         assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
         assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
@@ -216,21 +216,21 @@ public class GasStationControllerTests {
         assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
         assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
         assertEquals(gs.getLat(), gsDto.getLat());
-        assertEquals(gs.getLon(), gsDto.getLon());	
+        assertEquals(gs.getLon(), gsDto.getLon());
 	}*/
-	
+
 	@Test
 	public void testGetGasStationWithoutCoordinates() throws ClientProtocolException, IOException {
 		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getGasStationsWithoutCoordinates/Diesel/Enjoy");
 
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		String jsonFromResponse = EntityUtils.toString(response.getEntity());
-						
+
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		GasStationDto[] gs_array = mapper.readValue(jsonFromResponse, GasStationDto[].class);
 		GasStationDto gs = gs_array[0];
-				
+
 		assertNotNull(gs);
         assertEquals(gs.getGasStationId(), gsDto.getGasStationId());
         assertEquals(gs.getGasStationName(), gsDto.getGasStationName());
@@ -247,17 +247,17 @@ public class GasStationControllerTests {
         assertEquals(gs.getSuperPrice(), gsDto.getSuperPrice());
         assertEquals(gs.getSuperPlusPrice(), gsDto.getSuperPlusPrice());
         assertEquals(gs.getLat(), gsDto.getLat());
-        assertEquals(gs.getLon(), gsDto.getLon());	
+        assertEquals(gs.getLon(), gsDto.getLon());
 	}
-	
+
 	@Test
 	public void testSetGasStationReport() throws ClientProtocolException, IOException {
 		HttpPost request = new HttpPost("http://localhost:8080/gasstation/setGasStationReport/1/5/7/-1/-1/-1/1");
 		request.setHeader("Content-Type", "application/json");
-	    
+
 	    HttpResponse response = HttpClientBuilder.create().build().execute(request);
-	    
+
 		assertEquals(response.getStatusLine().getStatusCode(), 200);
-        
+
 	}
 }
